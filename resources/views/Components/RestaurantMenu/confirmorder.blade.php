d3624745697f1fed0058b38c1ad473017c15343c
Moshe Dayan <xynpyro@gmail.com>
2024-09-03 00:57:46 +0700

    feat : added logic to the confirm order, only the restaurant menu controller left

--- resources/views/Components/RestaurantMenu/confirmorder.blade.php
+++ resources/views/Components/RestaurantMenu/confirmorder.blade.php
@@ -14,12 +14,43 @@
 
             border-radius: .3rem;
         }
+
+        .primary-btn {
+            padding: 0.4rem 0.8rem 0.4rem 0.8rem;
+            border-radius: .4rem;
+            box-shadow: 0px 0.2rem 5px 0px rgba(0, 0, 0, 0.75);
+            -webkit-box-shadow: 0px 0.2rem 5px 0px rgba(0, 0, 0, 0.75);
+            -moz-box-shadow: 0px 0.2rem 5px 0px rgba(0, 0, 0, 0.75);
+            font-weight: 800;
+            color: #DB7352;
+        }
+
+        .glass-css {
+            background: white;
+            border-radius: 16px;
+            box-shadow: 0px 0.2rem 5px 0px rgba(0, 0, 0, 0.75);
+            -webkit-box-shadow: 0px 0.2rem 5px 0px rgba(0, 0, 0, 0.75);
+            -moz-box-shadow: 0px 0.2rem 5px 0px rgba(0, 0, 0, 0.75);
+        }
+
+        .yes {
+            position: relative;
+        }
+
+        .confirmation-page {
+            position: absolute;
+            top: -34rem;
+            display: none;
+            /* top: 120%;
+            left: 50%;
+            transform: translate(-50%, -90%); */
+        }
     </style>
 </head>
 
 <body>
     @include('Components.LandingComponent.header')
-    <div class="container">
+    <div class="container yes">
         <div class="row">
             <div class="col-7">
                 @foreach ($fQty as $index => $yes)
@@ -79,33 +110,124 @@
                             <h5 id="subtotal">Rp. {{ $subtotal }} k</h5>
                         </div>
                     </div>
-                    <div>
-                        <a href="" class="button">Order Now</a>
+                    <div class="d-flex justify-content-center mt-5">
+                        <a onclick="showDisplay()" class="primary-btn" style="cursor: pointer;">Order Now</a>
                     </div>
                 </div>
+            </div>
+        </div>
+
+
+        {{-- confirmation page --}}
+        <div class="container confirmation-page" id="confirm_page">
+            <div class="row">
+                <div class="col-3"></div>
+                <div class="col-6 d-flex p-4 flex-column align-items-center rounded-2 glass-css">
+                    <div class="d-flex justify-content-end ms-auto">
+                        <a style="cursor: pointer;" onclick="removeDisplay()" class="ms-auto">
+                            <h2 class="fw-bolder">x</h2>
+                        </a>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <h1 class="text-center">Order Summary</h1>
+                    </div>
+                    <div class="row d-flex align-items-center">
+                        @foreach ($fQty as $index => $yes)
+                            <div class="col-6">
+                                <div class="d-flex flex-column">
+                                    <h3 class="text-start">{{ $fName[$index] }}</h3>
+                                    <h5 class="text-start mt-2" id="qtys-{{ $index }}">{{ $fQty[$index] }}
+                                    </h5>
+                                </div>
+                            </div>
+                            <div class="col-6 d-flex flex-row justify-content-end">
+                                <h4>Rp. </h4>
+                                <h3 class="fw-bold" id="totals-{{ $index }}">{{ $fTot[$index] }}
+                                </h3>
+                                <h4>k</h4>
+                            </div>
+                        @endforeach
+                        <hr / class="mt-3">
+                        <div class="container-fluid mt-3 d-flex flex-row justify-content-between">
+                            <h4>Total</h4>
+                            <div class="d-flex flex-row">
+                                <h4>Rp.</h4>
+                                <h3 class="fw-bold" id="subtotals">{{ $subtotal }}</h3>
+                                <h4>k</h4>
+                            </div>
+                        </div>
+                        <hr / class="mt-2">
+                        <form action="" method="post">
+                            @csrf
+                            @method('post')
+                            @foreach ($fQty as $index => $yes)
+                                <input type="hidden" name="fId" id="fId-{{ $index }}"
+                                    value={{ $fId[$index] }}>
+                                <input type="hidden" name="fQty" id="fQty-{{ $index }}" value="">
+                            @endforeach
+                            <div class="d-flex justify-content-center">
+                                <a class="primary-btn" onclick="updateQty()">Confirm Order</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-3"></div>
             </div>
         </div>
     </div>
     @include('Components.LandingComponent.footer')
 
     <script>
+        function removeDisplay() {
+            var display = document.getElementById('confirm_page');
+            display.style.display = "none";
+        }
+
+        function showDisplay() {
+            var display = document.getElementById('confirm_page');
+            display.style.display = "block";
+        }
+
+        function updateQty() {
+            @foreach ($fQty as $index => $yes)
+                var qtyValue = document.getElementById('qty-{{ $index }}');
+                var qtyInput = document.getElementById('fQty-{{ $index }}');
+                var qty = parseInt(qtyValue.textContent);
+                qtyInput.value = qty;
+                console.log(qtyInput);
+            @endforeach
+        }
+
         function addItem(index) {
             var qtyElement = document.getElementById('qty-' + index);
             var qty = parseInt(qtyElement.textContent);
             qtyElement.textContent = qty + 1;
 
+            ///confirmation page
+            var qtysElement = document.getElementById('qtys-' + index);
+            var qtys = parseInt(qtysElement.textContent);
+            qtysElement.textContent = qty + 1;
+
             updateTotal(index, qty + 1);
+            updateSubtotal();
         }
 
         function subItem(index) {
             var qtyElement = document.getElementById('qty-' + index);
             var button = document.getElementById('sub-' + index);
             var qty = parseInt(qtyElement.textContent);
-            if (qty == 0) {
-                button.style.color = "gray";
-            } else {
+
+            ///confirmation page
+            var qtysElement = document.getElementById('qtys-' + index);
+            var qtys = parseInt(qtysElement.textContent);
+
+            if (qty > 0) {
                 qtyElement.textContent = qty - 1;
+                qtysElement.textContent = qty - 1;
                 updateTotal(index, qty - 1);
+                updateSubtotal();
+            } else {
+                button.style.color = "gray";
             }
         }
 
@@ -121,16 +243,32 @@
 
         function updateTotal(index, qty) {
             var priceElement = document.getElementById('total-' + index);
+            var totalElement = document.getElementById('totals-' + index);
             var price = @json($fPrice);
-            console.log(price[index]);
 
-            var yes = price[index] * qty;
-
-            priceElement.textContent = formatRupiah(yes);
+            var total = price[index] * qty;
+            priceElement.textContent = formatRupiah(total);
+            totalElement.textContent = formatRupiah(total);
         }
 
-        
+        function updateSubtotal() {
+            var subtotal = 0;
+            var prices = @json($fPrice);
+
+            @foreach ($fQty as $index => $yes)
+                var qty = parseInt(document.getElementById('qty-{{ $index }}').textContent);
+                subtotal += prices[{{ $index }}] * qty;
+            @endforeach
+
+            var subtotalElement = document.getElementById('subtotal');
+            subtotalElement.textContent = "Rp. " + formatRupiah(subtotal) + " k";
+
+            // confirmation pop up
+            var subtotalsElement = document.getElementById('subtotals');
+            subtotalsElement.textContent = formatRupiah(subtotal);
+        }
     </script>
+
 </body>
 
 </html>
