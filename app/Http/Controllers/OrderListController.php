220e1e4cb5ef1c2bca5bff46d7dbc56f564e355d
Moshe Dayan <xynpyro@gmail.com>
2024-09-10 18:47:17 +0700

    feat : fixed order list and whatever is inside or surrounding it

--- app/Http/Controllers/OrderListController.php
+++ app/Http/Controllers/OrderListController.php
@@ -11,256 +11,84 @@ class OrderListController extends Controller
     public function orderList()
     {
         // Fetch data for today's orders, all orders, and pending orders
-        $customerDataToday = DB::select("SELECT id, cust_name, cust_food_name, cust_quantity, customer_table, created_at, cust_total, status_id, cust_status FROM `order` WHERE DATE(created_at) = CURDATE() AND status_id != 4 AND status_id != 5");
-        $customerDataHistory = DB::select("SELECT id, cust_name, cust_food_name, cust_quantity, customer_table, created_at, cust_total, status_id, cust_status FROM `order` WHERE DATE(created_at)!= CURDATE() AND status_id = 4");
-        $customerDataPending = DB::select("SELECT id, cust_name, cust_food_name, cust_quantity, customer_table, created_at, cust_total, cust_status FROM `order` WHERE status_id = 1 AND DATE(created_at) = CURDATE()");
-        $customerDataConfirmed = DB::select("SELECT id, cust_name, cust_food_name, cust_quantity, customer_table, created_at, cust_total, cust_status FROM `order` WHERE status_id = 2 AND DATE(created_at) = CURDATE()");
-        $customerDataPreparing = DB::select("SELECT id, cust_name, cust_food_name, cust_quantity, customer_table, created_at, cust_total, cust_status FROM `order` WHERE status_id = 3 AND DATE(created_at) = CURDATE()");
-        $customerDataCompleted = DB::select("SELECT id, cust_name, cust_food_name, cust_quantity, customer_table, created_at, cust_total, cust_status FROM `order` WHERE status_id = 4 AND DATE(created_at) = CURDATE()");
-        $customerDataCancelled = DB::select("SELECT id, cust_name, cust_food_name, cust_quantity, customer_table, created_at, cust_total, cust_status FROM `order` WHERE status_id = 5 AND DATE(created_at) = CURDATE()");
-
+        $customerDataToday = DB::select("SELECT 
+                                        `order`.id,
+                                        `customer`.customer_name,
+                                        `menu`.f_name,
+                                        `detail_order`.food_qty,
+                                        `order`.created_at,
+                                        `menu`.f_price * `detail_order`.food_qty AS total_price,
+                                        `order`.status_id,
+                                        `order_status`.order_status
+                                        FROM
+                                        `order`
+                                        JOIN `detail_order` ON `order`.id = `detail_order`.order_id
+                                        JOIN `menu` ON `detail_order`.food_id = `menu`.id
+                                        JOIN `customer` ON `customer`.id = `order`.customer_id
+                                        JOIN `order_status` ON `order_status`.id = `order`.status_id
+                                        WHERE
+                                        DATE(`order`.created_at) = CURDATE()
+                                        AND 
+                                        `order`.status_id != 4
+                                        AND
+                                        `order`.status_id != 5");
         // Initialize today arrays to hold data
+        $dataId = [];
         $custIdToday = [];
         $custNameToday = [];
         $custFoodToday = [];
         $custQtyToday = [];
-        $custTableToday = [];
         $custCreatedAtToday = [];
         $custTotalToday = [];
         $custTotalRupiahToday = [];
         $custStatusToday = [];
-        // Initialize pending arrays to hold data
-        $custIdPending = [];
-        $custNamePending = [];
-        $custFoodPending = [];
-        $custQtyPending = [];
-        $custTablePending = [];
-        $custCreatedAtPending = [];
-        $custTotalPending = [];
-        $custTotalRupiahPending = [];
-        $custStatusPending = [];
-        // Initialize confirmed arrays to hold data
-        $custIdConfirmed = [];
-        $custNameConfirmed = [];
-        $custFoodConfirmed = [];
-        $custQtyConfirmed = [];
-        $custTableConfirmed = [];
-        $custCreatedAtConfirmed = [];
-        $custTotalConfirmed = [];
-        $custTotalRupiahConfirmed = [];
-        $custStatusConfirmed = [];
-        // Initialize preparing arrays to hold data
-        $custIdPreparing = [];
-        $custNamePreparing = [];
-        $custFoodPreparing = [];
-        $custQtyPreparing = [];
-        $custTablePreparing = [];
-        $custCreatedAtPreparing = [];
-        $custTotalPreparing = [];
-        $custTotalRupiahPreparing = [];
-        $custStatusPreparing = [];
-        // Initialize completed arrays to hold data
-        $custIdCompleted = [];
-        $custNameCompleted = [];
-        $custFoodCompleted = [];
-        $custQtyCompleted = [];
-        $custTableCompleted = [];
-        $custCreatedAtCompleted = [];
-        $custTotalCompleted = [];
-        $custTotalRupiahCompleted = [];
-        $custStatusCompleted = [];
-        // Initialize cancelled arrays to hold data
-        $custIdCancelled = [];
-        $custNameCancelled = [];
-        $custFoodCancelled = [];
-        $custQtyCancelled = [];
-        $custTableCancelled = [];
-        $custCreatedAtCancelled = [];
-        $custTotalCancelled = [];
-        $custTotalRupiahCancelled = [];
-        $custStatusCancelled = [];
-        // Initialize history arrays to hold data
-        $custIdHistory = [];
-        $custNameHistory = [];
-        $custFoodHistory = [];
-        $custQtyHistory = [];
-        $custTableHistory = [];
-        $custCreatedAtHistory = [];
-        $custTotalHistory = [];
-        $custTotalRupiahHistory = [];
-        $custStatusHistory = [];
+        $custStatusId = [];
+        $numbers = [];
+        $a = 1;
 
-        // Populate arrays for history orders
-        foreach ($customerDataHistory as $customers) {
-            $custIdHistory[] = $customers->id;
-            $custNameHistory[] = $customers->cust_name;
-            $custFoodHistory[] = $customers->cust_food_name;
-            $custQtyHistory[] = $customers->cust_quantity;
-            $custTableHistory[] = $customers->customer_table;
-            $custCreatedAtHistory[] = $customers->created_at;
-            $custTotalHistory[] = $customers->cust_total;
-            // Convert from Vanilla Money to IDR
-            $custTotalRupiahHistory[] = number_format($customers->cust_total, 0, ",", ".");
-            $custStatusHistory[] = $customers->cust_status;
-        }
-
-        // Populate arrays for cancelled orders
-        foreach ($customerDataCancelled as $customers) {
-            $custIdCancelled[] = $customers->id;
-            $custNameCancelled[] = $customers->cust_name;
-            $custFoodCancelled[] = $customers->cust_food_name;
-            $custQtyCancelled[] = $customers->cust_quantity;
-            $custTableCancelled[] = $customers->customer_table;
-            $custCreatedAtCancelled[] = $customers->created_at;
-            $custTotalCancelled[] = $customers->cust_total;
-            // Convert from Vanilla Money to IDR
-            $custTotalRupiahCancelled[] = number_format($customers->cust_total, 0, ",", ".");
-            $custStatusCancelled[] = $customers->cust_status;
-        }
-        // Populate arrays for completed orders
-        foreach ($customerDataCompleted as $customers) {
-            $custIdCompleted[] = $customers->id;
-            $custNameCompleted[] = $customers->cust_name;
-            $custFoodCompleted[] = $customers->cust_food_name;
-            $custQtyCompleted[] = $customers->cust_quantity;
-            $custTableCompleted[] = $customers->customer_table;
-            $custCreatedAtCompleted[] = $customers->created_at;
-            $custTotalCompleted[] = $customers->cust_total;
-            // Convert from Vanilla Money to IDR
-            $custTotalRupiahCompleted[] = number_format($customers->cust_total, 0, ",", ".");
-            $custStatusCompleted[] = $customers->cust_status;
-        }
-        // Populate arrays for preparing orders
-        foreach ($customerDataPreparing as $customers) {
-            $custIdPreparing[] = $customers->id;
-            $custNamePreparing[] = $customers->cust_name;
-            $custFoodPreparing[] = $customers->cust_food_name;
-            $custQtyPreparing[] = $customers->cust_quantity;
-            $custTablePreparing[] = $customers->customer_table;
-            $custCreatedAtPreparing[] = $customers->created_at;
-            $custTotalPreparing[] = $customers->cust_total;
-            // Convert from Vanilla Money to IDR
-            $custTotalRupiahPreparing[] = number_format($customers->cust_total, 0, ",", ".");
-            $custStatusPreparing[] = $customers->cust_status;
-        }
-        // Populate arrays for confirmed orders
-        foreach ($customerDataConfirmed as $customers) {
-            $custIdConfirmed[] = $customers->id;
-            $custNameConfirmed[] = $customers->cust_name;
-            $custFoodConfirmed[] = $customers->cust_food_name;
-            $custQtyConfirmed[] = $customers->cust_quantity;
-            $custTableConfirmed[] = $customers->customer_table;
-            $custCreatedAtConfirmed[] = $customers->created_at;
-            $custTotalConfirmed[] = $customers->cust_total;
-            // Convert from Vanilla Money to IDR
-            $custTotalRupiahConfirmed[] = number_format($customers->cust_total, 0, ",", ".");
-            $custStatusConfirmed[] = $customers->cust_status;
-        }
-        // Populate arrays for pending orders
-        foreach ($customerDataPending as $customers) {
-            $custIdPending[] = $customers->id;
-            $custNamePending[] = $customers->cust_name;
-            $custFoodPending[] = $customers->cust_food_name;
-            $custQtyPending[] = $customers->cust_quantity;
-            $custTablePending[] = $customers->customer_table;
-            $custCreatedAtPending[] = $customers->created_at;
-            $custTotalPending[] = $customers->cust_total;
-            // Convert from Vanilla Money to IDR
-            $custTotalRupiahPending[] = number_format($customers->cust_total, 0, ",", ".");
-            $custStatusPending[] = $customers->cust_status;
-        }
-        // Populate arrays for today's orders
-        foreach ($customerDataToday as $customers) {
-            $custIdToday[] = $customers->id;
-            $custNameToday[] = $customers->cust_name;
-            $custFoodToday[] = $customers->cust_food_name;
-            $custQtyToday[] = $customers->cust_quantity;
-            $custTableToday[] = $customers->customer_table;
+        foreach ($customerDataToday as $index => $customers) {
+            $dataId[] = $customers->id;
+            $custNameToday[] = $customers->customer_name;
+            $custFoodToday[] = $customers->f_name;
+            $custQtyToday[] = $customers->food_qty;
             $custCreatedAtToday[] = $customers->created_at;
-            $custTotalToday[] = $customers->cust_total;
+            $custTotalToday[] = $customers->total_price;
             // Convert from Vanilla Money to IDR
-            $custTotalRupiahToday[] = number_format($customers->cust_total, 0, ",", ".");
-            $custStatusToday[] = $customers->cust_status;
+            $custTotalRupiahToday[] = number_format($customers->total_price, 0, ",", ".");
+            $custStatusToday[] = $customers->order_status;
+            $custStatusId[] = $customers->status_id;
+            $numbers[] = $a;
+            if ($index > 0 && $dataId[$index] == $dataId[$index - 1]) {
+                $a++;
+            } else {
+            }
         }
 
         // Pass data to the view for today's orders
         return view("listorder", [
             // Today Data
+            "dataId" => $dataId,
+            "dataOrderStatus" => $custStatusId,
+            "dataNumber" => $numbers,
             "custIdToday" => $custIdToday,
-            "custNameToday" => $custNameToday,
-            "custFoodToday" => $custFoodToday,
-            "custQtyToday" => $custQtyToday,
-            "custTableToday" => $custTableToday,
-            "custCreatedAtToday" => $custCreatedAtToday,
-            "custTotalRupiahToday" => $custTotalRupiahToday,
+            "dataCustName" => $custNameToday,
+            "dataFoodName" => $custFoodToday,
+            "dataFoodQty" => $custQtyToday,
+            "dataCreatedAt" => $custCreatedAtToday,
+            "dataFoodSubtotal" => $custTotalRupiahToday,
             "custStatusToday" => $custStatusToday,
-            // Pending Data
-            "custIdPending" => $custIdPending,
-            "custNamePending" => $custNamePending,
-            "custFoodPending" => $custFoodPending,
-            "custQtyPending" => $custQtyPending,
-            "custTablePending" => $custTablePending,
-            "custCreatedAtPending" => $custCreatedAtPending,
-            "custTotalRupiahPending" => $custTotalRupiahPending,
-            "custStatusPending" => $custStatusPending,
-            // Confirmed Data
-            "custIdConfirmed" => $custIdConfirmed,
-            "custNameConfirmed" => $custNameConfirmed,
-            "custFoodConfirmed" => $custFoodConfirmed,
-            "custQtyConfirmed" => $custQtyConfirmed,
-            "custTableConfirmed" => $custTableConfirmed,
-            "custCreatedAtConfirmed" => $custCreatedAtConfirmed,
-            "custTotalRupiahConfirmed" => $custTotalRupiahConfirmed,
-            "custStatusConfirmed" => $custStatusConfirmed,
-            // Preparing Data
-            "custIdPreparing" => $custIdPreparing,
-            "custNamePreparing" => $custNamePreparing,
-            "custFoodPreparing" => $custFoodPreparing,
-            "custQtyPreparing" => $custQtyPreparing,
-            "custTablePreparing" => $custTablePreparing,
-            "custCreatedAtPreparing" => $custCreatedAtPreparing,
-            "custTotalRupiahPreparing" => $custTotalRupiahPreparing,
-            "custStatusPreparing" => $custStatusPreparing,
-            // Completed Data
-            "custIdCompleted" => $custIdCompleted,
-            "custNameCompleted" => $custNameCompleted,
-            "custFoodCompleted" => $custFoodCompleted,
-            "custQtyCompleted" => $custQtyCompleted,
-            "custTableCompleted" => $custTableCompleted,
-            "custCreatedAtCompleted" => $custCreatedAtCompleted,
-            "custTotalRupiahCompleted" => $custTotalRupiahCompleted,
-            "custStatusCompleted" => $custStatusCompleted,
-            // Cancelled Data
-            "custIdCancelled" => $custIdCancelled,
-            "custNameCancelled" => $custNameCancelled,
-            "custFoodCancelled" => $custFoodCancelled,
-            "custQtyCancelled" => $custQtyCancelled,
-            "custTableCancelled" => $custTableCancelled,
-            "custCreatedAtCancelled" => $custCreatedAtCancelled,
-            "custTotalRupiahCancelled" => $custTotalRupiahCancelled,
-            "custStatusCancelled" => $custStatusCancelled,
-            // History Data
-            "custIdHistory" => $custIdHistory,
-            "custNameHistory" => $custNameHistory,
-            "custFoodHistory" => $custFoodHistory,
-            "custQtyHistory" => $custQtyHistory,
-            "custTableHistory" => $custTableHistory,
-            "custCreatedAtHistory" => $custCreatedAtHistory,
-            "custTotalRupiahHistory" => $custTotalRupiahHistory,
-            "custStatusHistory" => $custStatusHistory,
         ]);
     }
 
     public function update(Request $request)
     {
-        // Retrieve the array of IDs and list of status updates
         $listIds = $request->input("index");
         $listCategories = $request->input("listUpdate");
 
-        // Iterate through each pair of ID and status update
+        // dd($listCategories);
         foreach ($listIds as $index => $listId) {
-            // Retrieve the corresponding status update for the current ID
             $listCategory = $listCategories[$index];
+            // dd($listCategory);
 
             if (!is_numeric($listCategory)) {
                 $selectId = DB::select("SELECT `id` FROM `order_status` WHERE `order_status` = '$listCategory'");
@@ -269,10 +97,6 @@ class OrderListController extends Controller
                 $designatedDataGet = DB::update("UPDATE `order` SET `status_id` = '$selectedId' WHERE `id` = $listId");
             } else {
                 $designatedDataGet = DB::update("UPDATE `order` SET `status_id` = $listCategory WHERE `id` = $listId");
-                $updateTheStatus = DB::update("UPDATE `order`
-                JOIN `order_status` ON `order`.status_id = `order_status`.id
-                SET `order`.cust_status = `order_status`.`order_status`
-                WHERE `order`.status_id = `order_status`.id");
             }
         }
         return redirect()->route("dashboard.list");
--- resources/views/Components/OrderList/list.blade.php
+++ resources/views/Components/OrderList/list.blade.php
@@ -45,19 +45,25 @@
                                 {{ $dataFoodName[$index] }}</td>
                             <td style="border: 1px solid #95a1f9">{{ $dataFoodQty[$index] }}</td>
                             <td style="border: 1px solid #95a1f9">Rp. {{ $dataFoodSubtotal[$index] }} k</td>
-                            <td style="border: 1px solid #95a1f9;">
+                            <td class="text-center" style="border: 1px solid #95a1f9;">
                                 {{ $dataCreatedAt[$index] }}</td>
-                            <input type="hidden" name="index[]" value="{{ $dataId[$index] }}">
-                            <td class="p-2">
-                                <select name="listUpdate[]" class="form-select form-select-sm">
-                                    <option selected>{{ $dataOrderStatus[$index] }}</option>
-                                    <option value="1">Pending</option>
-                                    <option value="2">Confirmed</option>
-                                    <option value="3">Preparing</option>
-                                    <option value="4" class="good-text">Completed</option>
-                                    <option value="5" class="bad-text">Cancelled</option>
-                                </select>
-                            </td>
+                            @if ($index === 0 || $dataCustName[$index] != $dataCustName[$index - 1])
+                                <input type="hidden" name="index[]" value="{{ $dataId[$index] }}">
+                            @else
+                            @endif
+                            @if ($index === 0 || $dataCustName[$index] != $dataCustName[$index - 1])
+                                <td class="p-2">
+                                    <select name="listUpdate[]" class="form-select form-select-sm">
+                                        <option selected>{{ $custStatusToday[$index] }}</option>
+                                        <option value="1">Pending</option>
+                                        <option value="2">Confirmed</option>
+                                        <option value="3">Preparing</option>
+                                        <option value="4" class="good-text">Completed</option>
+                                        <option value="5" class="bad-text">Cancelled</option>
+                                    </select>
+                                </td>
+                            @else
+                            @endif
                         </tr>
                     @endforeach
                 </tbody>
